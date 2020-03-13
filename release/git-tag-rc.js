7a944d16a02f6eeac54a634a12e9e2b62cb77fba
Paul Bredenberg <paulbredenberg@gmail.com>
2020-03-13 14:01:59 -0400

    build: ensure tagging happens before release

--- release/git-tag-rc.js
+++ release/git-tag-rc.js
@@ -3,9 +3,41 @@
 const { Plugin } = require('release-it');
 
 class GitTagRc extends Plugin {
-   async release() {
-      const context = this.config.getContext(),
-            newTag = `v${context.version}`;
+
+   getNewTag() {
+      const context = this.config.getContext();
+
+      return `v${context.version}`;
+   }
+
+   async addTag() {
+      const newTag = this.getNewTag();
+
+      this.log.log(`Tagging ${newTag}...`);
+
+      // Tag without annotation, which makes it easier when filtering
+      // commit for changelogs in major versions.
+      try {
+         return this.exec(`git tag ${newTag}`);
+      } catch(error) {
+         this.handleError(error);
+      }
+   }
+
+   async pushTag() {
+      const newTag = this.getNewTag();
+
+      this.log.log('Pushing tags...');
+
+      try {
+         return this.exec(`git push origin ${newTag}`);
+      } catch(error) {
+         this.handleError(error);
+      }
+   }
+
+   async beforeRelease() {
+      const newTag = this.getNewTag();
 
       // Set up our interactive prompts.
       this.registerPrompts({
@@ -14,7 +46,7 @@ class GitTagRc extends Plugin {
             message: () => { return `Tag this release with ${newTag}?`; },
          },
          pushTag: {
-            types: 'confirm',
+            type: 'confirm',
             message: () => { return `Push tag ${newTag}?`; },
          },
       });
@@ -23,27 +55,30 @@ class GitTagRc extends Plugin {
       await this.step(
          {
             enabled: true,
+            task: () => { return this.addTag(); },
             label: 'Prompt for tagging.',
             prompt: 'tagRc',
          }
       );
 
-      this.log.log(`Tagging ${newTag}...`);
-      // Tag without annotation, which makes it easier when filtering
-      // commit for changelogs in major versions.
-      this.exec(`git tag ${newTag}`);
-
       // Push the tag.
       await this.step(
          {
             enabled: true,
+            task: () => { return this.pushTag(); },
             label: 'Prompt for tag push.',
             prompt: 'pushTag',
          }
       );
+   }
 
-      this.log.log('Pushing tags...');
-      this.exec(`git push origin ${this.getNewTag()}`);
+   rollBack() {
+      return this.exec(`git tag -d ${this.getNewTag()}`);
+   }
+
+   handleError(error) {
+      this.debug(error);
+      this.rollBack(this.getNewTag());
    }
 }
 
