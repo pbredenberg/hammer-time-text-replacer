0db0ecb59e3700291b1cef120c36a58181fa5e2f
Paul Bredenberg <paulbredenberg@gmail.com>
2020-03-13 10:49:58 -0400

    feat: support restore button functionality

--- src/Replacer.ts
+++ src/Replacer.ts
@@ -1,11 +1,22 @@
 interface IReplacerConfig {
    buttonSelectors?: string[];
    replacementSelectors?: string[];
+   restoreButtonSelectors?: string[];
+   replaceButtonSelectors?: string[];
 }
+
+interface INodeRecord {
+   id: number;
+   node: Node;
+}
+
+const DEFAULT_REPLACEMENT_SELECTOR = '.replace-me';
 
 export default class Replacer {
 
    private _config: IReplacerConfig = {};
+
+   private _nodeRecords: INodeRecord[] = [];
 
    public constructor(config: IReplacerConfig = {}) {
       this._config = config;
@@ -17,25 +28,53 @@ export default class Replacer {
     * Performs repacements.
     */
    public replace(): void {
-      const selectors = this._config.replacementSelectors || [ '.replace-me' ];
+      const selectors = this._config.replacementSelectors || [ DEFAULT_REPLACEMENT_SELECTOR ];
 
       document
          .querySelectorAll(this._selectorsJoiner(selectors))
          .forEach((el) => {
             if (el.textContent) {
+               this._nodeRecords.push({
+                  id: this._nodeRecords.length,
+                  node: el.cloneNode(true),
+               });
                el.textContent = 'Hammer time.';
             }
          });
    }
 
-   private _initButtons(): void {
-      const selectors = this._config.buttonSelectors || [ '.replace-button' ];
+   public restore(): void {
+      const selectors = this._config.replacementSelectors || [ DEFAULT_REPLACEMENT_SELECTOR ];
 
       document
          .querySelectorAll(this._selectorsJoiner(selectors))
+         .forEach((el, key) => {
+            const nodeRecord = this._nodeRecords[key];
+
+            if (nodeRecord) {
+               el.textContent = nodeRecord.node.textContent;
+            }
+         });
+   }
+
+   private _initButtons(): void {
+      const replaceButtonSelectors = this._config.replaceButtonSelectors || [ '.replace-button' ];
+
+      const restoreButtonSelectors = this._config.restoreButtonSelectors || [ '.restore-button' ];
+
+      document
+         .querySelectorAll(this._selectorsJoiner(replaceButtonSelectors))
          .forEach((button) => {
             button.addEventListener('click', () => {
                this.replace();
+            });
+         });
+
+      document
+         .querySelectorAll(this._selectorsJoiner(restoreButtonSelectors))
+         .forEach((button) => {
+            button.addEventListener('click', () => {
+               this.restore();
             });
          });
    }
